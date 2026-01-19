9e4f0c93bf483c96e05c7cbe9b3ae280a96533d5
Al-fay <diva3577@gmail.com>
2026-01-19 14:10:52 +0700

    third commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://github.com/Al-fay/belajar-astro-blog",
+});
