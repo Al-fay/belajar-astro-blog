4224c359546042b14f7b46d1a2e9e68f8f62f6d6
Al-fay <diva3577@gmail.com>
2026-01-19 16:16:11 +0700

    fourth commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/Al-fay/belajar-astro-blog",
+  site: "https://resilient-torrone-8ae3d6.netlify.app/",
+  integrations: [preact()],
 });
